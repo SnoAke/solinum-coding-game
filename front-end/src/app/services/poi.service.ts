90110a9f1188cc6e5e288ba945918947b6b441f6
SnoAke <pierre.blanchais@free.fr>
2020-08-06 19:43:41 +0200

    Connect the poi service with the backend API

--- front-end/src/app/services/poi.service.ts
+++ front-end/src/app/services/poi.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+
+const baseUrl = 'http://localhost:8080/api/poi';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class PoiService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  create(data: Object): Observable<any> {
+    return this.http.post(baseUrl, data);
+  }
+
+  getPublished(): Observable<any> {
+    return this.http.get(baseUrl);
+  }
+
+  getDraft(): Observable<any> {
+    return this.http.get(`${baseUrl}/draft`);
+  }
+
+  getOne(id: String): Observable<any> {
+    return this.http.get(`${baseUrl}/${id}`)
+  }
+
+  update(id: String, data: Object): Observable<any> {
+    return this.http.put(`${baseUrl}/${id}`, data);
+  }
+
+  changeStatus(id: String, data: Object): Observable<any> {
+    return this.http.patch(`${baseUrl}/${id}`, data);
+  }
 }
