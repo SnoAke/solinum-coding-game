8959648094270cc9ed3a7ee7495c578edda29e42
SnoAke <pierre.blanchais@free.fr>
2020-08-07 10:21:17 +0200

    Implementation of the add-poi template and controller (without auto-completion of the address)

--- front-end/src/app/components/add-poi/add-poi.component.ts
+++ front-end/src/app/components/add-poi/add-poi.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PoiService } from '../../services/poi.service';
 
 @Component({
   selector: 'app-add-poi',
@@ -7,9 +8,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddPoiComponent implements OnInit {
 
-  constructor() { }
+  poi = {
+    name: "",
+    full_address: "",
+    longitude: "",
+    latitude: "",
+    type: "",
+    poster_email: ""
+  };
+  submitted = false;
+
+  constructor( private poiService: PoiService ) { }
 
   ngOnInit(): void {
+  }
+
+  savePoi(): void {
+    const data = {
+      name: this.poi.name,
+      address: {
+        full_address: this.poi.full_address,
+        longitude: this.poi.longitude,
+        latitude: this.poi.latitude,
+      },
+      type: this.poi.type,
+      poster_email: this.poi.poster_email
+    };
+
+    this.poiService.create(data)
+      .subscribe(
+        response => {
+          console.log(response);
+          this.submitted = true;
+        },
+        error => {
+          console.log(error);
+        });
+  }
+
+  newPoi(): void {
+    this.submitted = false;
+    this.poi = {
+      name: "",
+      full_address: "",
+      longitude: "",
+      latitude: "",
+      type: "",
+      poster_email: ""
+    };
   }
 
 }
