040a1536feef1d538da4856bed822b045dec816b
SnoAke <pierre.blanchais@free.fr>
2020-08-08 14:00:10 +0200

    Add a message when there is no poi

--- front-end/src/app/components/poi-list/poi-list.component.ts
+++ front-end/src/app/components/poi-list/poi-list.component.ts
@@ -10,7 +10,7 @@ import { Router } from "@angular/router";
 export class PoiListComponent implements OnInit {
 
   statePoi;
-  pois: any;
+  pois=[];
 
   constructor(
     private poiService: PoiService,
