9ee66c1e46edb98c3091eabf6b2f6c2cecf89a67
SnoAke <pierre.blanchais@free.fr>
2020-08-08 13:40:11 +0200

    Add a redirection from / to /poi

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { AddPoiComponent } from './components/add-poi/add-poi.component';
 
 
 const routes: Routes = [
+  { path: '', redirectTo: 'poi', pathMatch: 'full' },
   { path: 'poi', component: PoiListComponent },
   { path: 'draft', component: PoiListComponent },
   { path: 'poi/:id', component: PoiDetailsComponent },
